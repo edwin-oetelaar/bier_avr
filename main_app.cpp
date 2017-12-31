b86186788b749d1b41916bd0218166f7a6c98bf1
Edwin van den Oetelaar <edwin@oetelaar.com>
2017-12-31 11:10:31 +0100

    working! test input voltage high/low limits, switch off outputs when out of range - todo add timer to cycle power every 24 hr hours

--- main_app.cpp
+++ main_app.cpp
@@ -12,15 +12,15 @@
 // #include <math.h>
 #include "wi_private.h"
 #include "arduino.h"
-#include "LiquidCrystal.h"
+// #include "LiquidCrystal.h"
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 // Declared weak in Arduino.h to allow user redefinitions.
 int atexit(void (*/*func*/)()) { return 0; }
 void yield(void){};
 
-const uint32_t millis_in_24hr =  60 * 60 * 24 * 1000;
-const uint32_t adc_to_volt = 173;
+const uint32_t millis_in_24hr = 60 * 60 * 24 * 1000;
+const uint32_t adc_to_volt = 173; /* multiplier: get_adc_value * adc_to_volt = volts (times 1E4) */
 
 // constants won't change. Used here to set a pin number:
 const int ledPin = LED_BUILTIN; // the number of the LED pin
@@ -73,6 +73,7 @@ uint8_t prev_output_state[2] = {0};            /* previous state of leds */
 
 static volatile uint8_t device_state = 0; /* after power on, we start here */
 static uint8_t teller = 0;                // teller voor in state 1
+static uint8_t range_counter = 0;         /* count adc values in range */
 
 // Variables will change:
 int ledState = LOW; // ledState used to set the LED
@@ -84,12 +85,26 @@ unsigned long previousMillis = 0; // will store last time LED was updated
 // constants won't change:
 const long interval = 250; // interval at which to blink (milliseconds)
 // const int rs = 12, en = 11, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
-LiquidCrystal lcd; //(void); // rs, en, d4, d5, d6, d7);
+// LiquidCrystal lcd; //(void); // rs, en, d4, d5, d6, d7);
+
+uint8_t in_range(int val, int min, int max)
+{
+    if (val > max)
+        return 0;
+    if (val < min)
+        return 0;
+    return 1;
+}
 
 void timercallbacks(void)
 { /* dit wordt iedere x ms aangeroepen */
     uint8_t changed = 0;
-    /* controleer of user led status veranderde sinds vorige keer */
+    /* controleer of user led status veranderde sinds vorige keer
+     * een routine mag enkel de globale variablen updaten die worden dan 
+     * in deze timer callback ineens naar de outputs doorgestuurd, 
+     * een routine mag niet rechtstreeks IO sturen, 
+     * de mapping moet in deze functie gebeuren
+     */
     for (uint8_t i = 0; i < ARRAY_SIZE(output_state); i++)
     {
         if (prev_output_state[i] != output_state[i])
@@ -140,26 +155,25 @@ void timercallbacks(void)
         }
     }
 
-    int ch = Serial.read();
-    if (ch < 0)
-    {
-        /* nothing*/
-    }
-    else
-    {
-        if (ch == 65)
-        {
-            output_state[0] = 0;
-        }
-    }
+    // int ch = Serial.read();
+    // if (ch < 0)
+    // {
+    //     /* nothing*/
+    // }
+    // else
+    // {
+    //     if (ch == 65)
+    //     {
+    //         output_state[0] = 0;
+    //     }
+    // }
 }
-
-
 
 void setup()
 {
     DDRC = 0xFF;  //PC as output
     PORTC = 0x00; //keep all LEDs off
+    DDRE = 0x00;  // E is all inputs
     Serial.begin(9600);
     // lcd.begin(16, 2);
     //  lcd.print("hello folks");
@@ -212,7 +226,9 @@ uint16_t get_adc_value()
     // ADSC is cleared when the conversion finishes
     while (bit_is_set(ADCSRA, ADSC))
     {
-        /* empty wait */
+        /* empty wait 
+         * should do something else here instead of busy waiting 
+         * */
     };
 
     // we have to read ADCL first; doing so locks both ADCL
@@ -247,34 +263,39 @@ void loop()
 
     case 1:
         /* led blinking and power checking state */
-        if (currentMillis - previousMillis > interval)
+        /* check de spanning */
+        /* ga naar state 2 als de spanning voor het eerst in range is tussen 10.5 (660) en 14.5 (840)  volt */
+
+        if (currentMillis - previousMillis > 100L)
         {
             // save the last time you blinked the LED
             previousMillis = currentMillis;
             teller += 1; // tel de pulsen van de led
             // if the LED is off turn it on and vice-versa:
             ledState = (ledState == LOW) ? HIGH : LOW;
-            // {
-            //     ledState = HIGH;
-            // }
-            // else
-            // {
-            //     ledState = LOW;
-            // }
 
             output_state[0] = ledState;
 
-            Serial.println(previousMillis);
+            // Serial.println(previousMillis);
             //   lcd.setCursor(0, 1);
             //   lcd.print(previousMillis);
             /* read a value from the ADC input 7 */
             uint16_t adc_value = get_adc_value();
+            if (in_range(adc_value, 660, 840))
+            {
+                // in range wil ik 10 keer zien alvorens door te gaan
+                range_counter += 1;
+            }
+            else
+            {
+                range_counter = 0; /* we beginnen vooraan met tellen */
+            }
             //   lcd.setCursor(8, 1);
             //   lcd.print(adc_value);
             //   lcd.print("    ");
         }
 
-        if (teller == 10)
+        if (teller > 20 && range_counter > 10)
             ns = 2;
 
         break;
@@ -289,11 +310,45 @@ void loop()
             output_state[0] = ledState;
             // Serial.println(previousMillis);
             uint16_t adc_value = get_adc_value();
-            Serial.println(adc_value  *  adc_to_volt); // 117813 => 11.7813 Volt
+            Serial.println(adc_value * adc_to_volt); // 117813 => 11.7813 Volt
+            if (in_range(adc_value, 660, 840))
+            {
+                // alles ok
+                output_state[1] = HIGH; // fet on
+            }
+            else
+            {
+                // niet ok, zet alles uit, wacht tot spanning terugkomt
+                output_state[1] = LOW; // fet UIT
+                ns = 3;
+            }
         }
 
-         output_state[1] = HIGH; // fet on
-        
+        break;
+
+    case 3: /* power is out of range */
+        if (currentMillis - previousMillis > 100L)
+        {
+            // save the last time you blinked the LED, slow blinking
+            previousMillis = currentMillis;
+
+            ledState = (ledState == LOW) ? HIGH : LOW;
+            output_state[0] = ledState;
+            // Serial.println(previousMillis);
+            uint16_t adc_value = get_adc_value();
+            Serial.println(adc_value * adc_to_volt); // 117813 => 11.7813 Volt
+            if (in_range(adc_value, 660, 840))
+            {
+                // alles ok ga naar 1
+                ns = 0;
+            }
+            else
+            {
+                // niet ok, zet alles uit, wacht tot spanning terugkomt
+                ns = 3;
+            }
+        }
+
         break;
     }
 
